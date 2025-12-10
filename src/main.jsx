3000f7d2b3ed0e23a81e433ab90997b3b3648b95
k-praneeth567 <praneethkothamaddi567@gmail.com>
2025-12-11 00:26:18 +0530

    Wrap App component with BrowserRouter and Routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path="*" element={<App />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
